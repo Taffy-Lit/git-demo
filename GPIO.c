0d02634f33c16a2852e6cfcb5fedf1481df3d876
Taffy-Lit <wjh2890617474@163.com>
2026-06-27 11:43:20 +0800

    add GPIO.c

---
+++ GPIO.c
@@ -0,0 +1,8 @@
+GPIO_Init();
+if (Read_GPIO_Pin(GPIO_PIN_0) == GPIO_PIN_SET) {
+    // Pin is high
+    GPIO_WritePin(GPIO_PIN_1, GPIO_PIN_SET); // Set another pin high
+} else {
+    // Pin is low
+    GPIO_WritePin(GPIO_PIN_1, GPIO_PIN_RESET); // Set another pin low
+}
