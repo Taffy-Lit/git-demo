761bcf6cc6362d27c472ef4619fbfc9b6df3b22e
Taffy-Lit <wjh2890617474@163.com>
2026-06-27 11:39:08 +0800

    去除ab比较。感谢

--- c1.py
+++ c1.py
@@ -3,11 +3,5 @@ a = 1
 b =3
 print(a+b)
 print("Hello, World!")
-
-if a > b:
-    print("a is greater than b")
-
-if a < b:
-    print("a is less than b")
-
-print("Hello")
+print("Hello, World!")
+print("Thank you!")
